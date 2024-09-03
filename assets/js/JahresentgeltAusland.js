9b11f273b394a02865dccb3fc1f9ff498c1bf14e
mrmll3 <info@malleike.de>
2024-09-03 23:10:21 +0200

    update

--- assets/js/JahresentgeltAusland.js
+++ assets/js/JahresentgeltAusland.js
@@ -31,93 +31,86 @@ async function init_JahresentgeltAusland() {
     event.preventDefault();
     fetch_Bundesbank(JahresentgeltAusland_input_land.value);
   })
+  JahresentgeltAusland_input_entgelt.value = 1;
+  // load coutries that are supported by the Bundesbank API
   let path = "../assets/data/Jahreswechselkurse EUR - Bundesbank_de.json";
-  fetch(path)
-    .then(response => response.text())
-    .then(json => {
-      JahresentgeltAusland_zeitreihen = JSON.parse(json);
-      JahresentgeltAusland_laender = Object.keys(JahresentgeltAusland_zeitreihen).sort();
-      JahresentgeltAusland_laender.forEach(land => {
-        let option = document.createElement('option');
-        option.value = land;
-        JahresentgeltAusland_datalist_laender.appendChild(option);
-      })
-    })
-    .then(() => {
-      JahresentgeltAusland_aktuelles_land = JahresentgeltAusland_laender[0];
-      JahresentgeltAusland_input_land.value = JahresentgeltAusland_aktuelles_land;
-      JahresentgeltAusland_input_jahr.value = 2023;
-      fetch_Bundesbank(JahresentgeltAusland_aktuelles_land);
-    });
-
+  let response = await fetch(path);
+  let json = await response.json();
+  JahresentgeltAusland_zeitreihen = json;
+  JahresentgeltAusland_laender = Object.keys(JahresentgeltAusland_zeitreihen).sort();
+  JahresentgeltAusland_laender.forEach(land => {
+    let option = document.createElement('option');
+    option.value = land;
+    JahresentgeltAusland_datalist_laender.appendChild(option);
+  })
+  JahresentgeltAusland_aktuelles_land = JahresentgeltAusland_laender[0];
+  JahresentgeltAusland_input_land.value = JahresentgeltAusland_aktuelles_land;
+  JahresentgeltAusland_input_jahr.value = 2023;
+  fetch_Bundesbank(JahresentgeltAusland_aktuelles_land);
 }
 
 init_JahresentgeltAusland();
+
+
 // temporal function, will be splitted
 async function fetch_Bundesbank(land) {
+  let waehrung = JahresentgeltAusland_zeitreihen[land].Waehrung;
+  JahresentgeltAusland_Land.name = land;
+  JahresentgeltAusland_Land.jahreswerte = [];
+  await fetch_Bundesbank_data(land);
+
+  let entry = JahresentgeltAusland_Land.jahreswerte.find(o => o.jahr === JahresentgeltAusland_input_jahr.value)
+  let kurs = entry.wert;
+  let entgelt_fremd = Number(JahresentgeltAusland_input_entgelt.value);
+  let entgelt_euro = (Number(entgelt_fremd) / Number(kurs)).toFixed(2);
+  let result_text = `der aktuelle Kurs ist: ${kurs}. Das heißt es gibt für 1,00 € ${kurs} ${waehrung}.
+      Somit ergibt sich bei einem Entgelt von ${entgelt_fremd.toFixed(2)} ${waehrung} ein Entgelt in Euro von ${entgelt_euro}.`;
+  JahresentgeltAusland_result.innerHTML = result_text;
+
+
+  // console.log(await fetch_Bundesbank_data(land));
+
+  for await (const c_land of JahresentgeltAusland_laender) {
+    await JahresentgeltAusland_update_zeitreihen(c_land);
+  }
+
+  console.log(JahresentgeltAusland_zeitreihen);
+}
+
+
+async function fetch_Bundesbank_data(land) {
+  // TODO:  check if already loaded
+  //        if already loaded, use cache
   let waehrung = JahresentgeltAusland_zeitreihen[land].Waehrung;
   let url = JahresentgeltAusland_api_url + waehrung + JahresentgeltAusland_api_url_appendix;
   let xml;
   JahresentgeltAusland_Land.name = land;
   JahresentgeltAusland_Land.jahreswerte = [];
-  fetch(url)
-    .then(response => response.text())
-    .then(text => {
-      let parser = new DOMParser();
-      xml = parser.parseFromString(text, "text/xml");
-      xml = xml.firstChild.lastChild.firstChild.childNodes;
-      JahresentgeltAusland_datalist_jahre.innerHTML = "";
-      xml.forEach(element => {
-        if (!element.children[0].hasAttribute("id")) {
-          let jahr = element.children[0].attributes['value'].value;
-          let wert = element.children[1].attributes['value'].value;
-          JahresentgeltAusland_Land.jahreswerte.push({ "jahr": jahr, "wert": wert });
-          let option = document.createElement('option');
-          option.value = jahr;
-          JahresentgeltAusland_datalist_jahre.appendChild(option);
-        }
-      });
-    })
-    .then(() => {
-      let entry = JahresentgeltAusland_Land.jahreswerte.find(o => o.jahr === JahresentgeltAusland_input_jahr.value)
-      let kurs = entry.wert;
-      let entgelt_fremd = Number(JahresentgeltAusland_input_entgelt.value);
-      let entgelt_euro = (Number(entgelt_fremd) / Number(kurs)).toFixed(2);
-      let result_text = `der aktuelle Kurs ist: ${kurs}. Das heißt es gibt für 1,00 € ${kurs} ${waehrung}.
-      Somit ergibt sich bei einem Entgelt von ${entgelt_fremd.toFixed(2)} ${waehrung} ein Entgelt in Euro von ${entgelt_euro}.`;
-      JahresentgeltAusland_result.innerHTML = result_text;
+  let response = await fetch(url);
+  let text = await response.text();
 
-    });
-}
+  let parser = new DOMParser();
+  xml = parser.parseFromString(text, "text/xml");
+  xml = xml.firstChild.lastChild.firstChild.childNodes;
+  JahresentgeltAusland_datalist_jahre.innerHTML = "";
+  xml.forEach(element => {
+    if (!element.children[0].hasAttribute("id")) {
+      // console.log(element);
+      let jahr = element.children[0].attributes['value'].value;
+      let wert
+      try {
+        wert = element.children[1].attributes['value'].value;
 
-
-async function fetch_Bundesbank_data(land) {
-  let url = JahresentgeltAusland_api_url + JahresentgeltAusland_zeitreihen[land].Waehrung + JahresentgeltAusland_api_url_appendix;
-  let xml;
-  JahresentgeltAusland_Land.name = land;
-  JahresentgeltAusland_Land.jahreswerte = [];
-  fetch(url)
-    .then(response => response.text())
-    .then(text => {
-      let parser = new DOMParser();
-      xml = parser.parseFromString(text, "text/xml");
-      xml = xml.firstChild.lastChild.firstChild.childNodes;
-      JahresentgeltAusland_datalist_jahre.innerHTML = "";
-      xml.forEach(element => {
-        if (!element.children[0].hasAttribute("id")) {
-          let jahr = element.children[0].attributes['value'].value;
-          let wert = element.children[1].attributes['value'].value;
-          JahresentgeltAusland_Land.jahreswerte.push({ "jahr": jahr, "wert": wert });
-          let option = document.createElement('option');
-          option.value = jahr;
-          JahresentgeltAusland_datalist_jahre.appendChild(option);
-        }
-      });
-    })
-    .then(() => {
-      let entry = JahresentgeltAusland_Land.jahreswerte.find(o => o.jahr === JahresentgeltAusland_input_jahr.value)
-      JahresentgeltAusland_result.innerHTML = entry.wert;
-    });
+      } catch (error) {
+        console.error(`kein Kurs angegeben für ${waehrung} / ${land} im Jahr ${jahr}. Bitte manuell prüfen. Der Kurst wird auf 0.01 festgelegt`)
+        wert = 0.01;
+      }
+      JahresentgeltAusland_Land.jahreswerte.push({ "jahr": jahr, "wert": wert });
+      let option = document.createElement('option');
+      option.value = jahr;
+      JahresentgeltAusland_datalist_jahre.appendChild(option);
+    }
+  })
 
   return JahresentgeltAusland_Land.jahreswerte;
 
@@ -128,9 +121,8 @@ async function JahresentgeltAusland_update_zeitreihen(land) {
   // add empty array to object
   // if existing, it will be updated to empty array
   JahresentgeltAusland_zeitreihen[land]["jahreswerte"] = [];
-  let data = await fetch_Bundesbank_data();
+  let data;
+  data = await fetch_Bundesbank_data(land);
   JahresentgeltAusland_zeitreihen[land]["jahreswerte"] = data;
-
-
 
 }
