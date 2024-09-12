dd109de13e144667c88b7b95ab6ae0ea94a146f1
mrmll3 <info@malleike.de>
2024-09-12 14:07:56 +0200

    update Jahresentgelt

--- assets/js/JahresentgeltAusland.js
+++ assets/js/JahresentgeltAusland.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// region global vars
 const JahresentgeltAusland_section = document.querySelector("#JahresentgeltAusland");
 const JahresentgeltAusland_form = document.querySelector("#JahresentgeltAusland-form");
 const JahresentgeltAusland_input_land = document.querySelector("#JahresentgeltAusland-land");
@@ -7,7 +8,10 @@ const JahresentgeltAusland_datalist_laender = document.querySelector("#Jahresent
 const JahresentgeltAusland_datalist_jahre = document.querySelector("#JahresentgeltAusland-jahre");
 const JahresentgeltAusland_input_jahr = document.querySelector('#JahresentgeltAusland-jahr');
 const JahresentgeltAusland_input_entgelt = document.querySelector('#JahresentgeltAusland-entgelt');
+const JahresentgeltAusland_input_kurs = document.querySelector('#JahresentgeltAusland-kurs');
+const JahresentgeltAusland_input_kurs_enable = document.querySelector('#JahresentgeltAusland-kurs-enable');
 const JahresentgeltAusland_result = document.querySelector("#JahresentgeltAusland-result");
+
 
 const path_wechselkurse = "../assets/data/Jahreswechselkurse.json";
 const JahresentgeltAusland_api_url = "https://api.statistiken.bundesbank.de/rest/data/BBEX3/A."
@@ -16,7 +20,7 @@ const JahresentgeltAusland_api_url_appendix = ".EUR.BB.AC.A04?&detail=dataonly"
 let JahresentgeltAusland_zeitreihen;
 let JahresentgeltAusland_laender;
 
-
+// region init
 async function init_JahresentgeltAusland() {
   addEventListeners();
   // load coutries that are supported by the Bundesbank API
@@ -36,25 +40,45 @@ function addEventListeners() {
       event.target.select();
     }
   })
+
   JahresentgeltAusland_section.addEventListener("change", event => {
-    if (event.target.tagName === "INPUT" && event.target.id === "JahresentgeltAusland-land") {
-      update_datalist_jahre(event.target.value);
+    if (event.target.id === "JahresentgeltAusland-land") { update_datalist_jahre(event.target.value); }
+    if (event.target.id === "JahresentgeltAusland-kurs-enable") { JahresentgeltAusland_input_kurs.disabled = !JahresentgeltAusland_input_kurs_enable.checked; }
+    if (event.target.id === "JahresentgeltAusland-jahr" && (JahresentgeltAusland_input_kurs.disabled || JahresentgeltAusland_input_kurs.value == "")) {
+      if (JahresentgeltAusland_input_entgelt.value == "") { JahresentgeltAusland_input_entgelt.value = 1; }
+      update_result(JahresentgeltAusland_input_land.value);
     }
   })
+
   JahresentgeltAusland_form.addEventListener("submit", (event) => {
     event.preventDefault();
     update_result(JahresentgeltAusland_input_land.value);
   })
 }
 
+// region functions
 async function update_result(land) {
   let jahreswerte;
-  jahreswerte = await fetch_Bundesbank_data(land);
-  let entry = jahreswerte.find(o => o.jahr === JahresentgeltAusland_input_jahr.value)
-  let kurs = entry.wert;
+  let entry;
+  let kurs;
+  let jahr = JahresentgeltAusland_input_jahr.value ?? 0;
+  try {
+    jahreswerte = await fetch_Bundesbank_data(land);
+    // falls ein Jahr eingegeben wurde, das nicht in der Liste ist
+    entry = jahreswerte.find(obj => obj.jahr === jahr) ?? { "jahr": jahr, "wert": "1" };
+  } catch (error) {
+  }
+  if (JahresentgeltAusland_input_kurs_enable.checked) {
+    kurs = Number(JahresentgeltAusland_input_kurs.value);
+  }
+  else {
+    kurs = entry.wert;
+    JahresentgeltAusland_input_kurs.value = Number(kurs);
+  }
+
   let entgelt_fremd = Number(JahresentgeltAusland_input_entgelt.value);
   let entgelt_euro = (Number(entgelt_fremd) / Number(kurs)).toFixed(2);
-  let waehrung = JahresentgeltAusland_zeitreihen[land].Waehrung;
+  let waehrung = JahresentgeltAusland_zeitreihen[land].Waehrung ?? "Fremdwährung";
   let result_text =
     `Der aktuelle Kurs ist: ${kurs}. <br>
   Das heißt es gibt für 1,00 € ${kurs} ${waehrung}. <br> <br>
@@ -92,12 +116,16 @@ async function fetch_Bundesbank_data(land) {
       }
     }
   })
+  // should not be done here
+  // appends fetched data to local data for caching
   JahresentgeltAusland_update_zeitreihen(land, jahreswerte);
   return jahreswerte;
 
 }
 
 function update_datalist(datalist, options) {
+  // delete old options
+  datalist.innerHTML = "";
   for (const element of options) {
     let option = document.createElement('option');
     option.value = element;
@@ -117,5 +145,4 @@ async function JahresentgeltAusland_update_zeitreihen(land, data) {
   // if existing, it will be updated to empty array
   JahresentgeltAusland_zeitreihen[land]["jahreswerte"] = [];
   JahresentgeltAusland_zeitreihen[land]["jahreswerte"] = data;
-
 }
